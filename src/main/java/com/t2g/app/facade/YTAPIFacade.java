9c01ac944b9cbbcf1056bd6ddc8a77d2d2195fd4
heywtl <lincolnams4@gmail.com>
2022-01-09 20:29:13 -0300

    adding authentication

--- src/main/java/com/t2g/app/facade/YTAPIFacade.java
+++ src/main/java/com/t2g/app/facade/YTAPIFacade.java
@@ -1,14 +1,15 @@
 package com.t2g.app.facade;
 
 import com.google.gson.*;
+import com.t2g.app.dao.UserCredentialsTableDAO;
 import com.t2g.app.model.Playlist;
 import com.t2g.app.model.Song;
 import com.t2g.app.model.StreamingService;
 import com.t2g.app.model.UserCredentialsTableEntry;
 import com.t2g.app.model.YTSong;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -26,6 +27,9 @@ public class YTAPIFacade extends StreamingServiceFacade<YTSong>{
     private static final String PLAYLIST_CONTENT_BY_ID_ENDPOINT_TEMPLATE = "playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=%s";
     private static final String PLAYLIST_CREATE_ENDPOINT_TEMPLATE = "playlists?part=snippet";
     private static final String PLAYLIST_CREATE_CONTENT_ENDPOINT_TEMPLATE = "playlistItems?part=snippet";
+
+    @Autowired
+    private UserCredentialsTableDAO userCredentialsTableDAO;
 
     public YTAPIFacade() {
         super(100);
@@ -117,33 +121,75 @@ public class YTAPIFacade extends StreamingServiceFacade<YTSong>{
 
     @Override
     public void createPlaylist(String userId, Playlist playlist) throws Exception {
+        UserCredentialsTableEntry userCredentialsTableEntry = userCredentialsTableDAO.
+                getByUserIdAndService(userId, StreamingService.YOUTUBE);
+
         String body = String
-                .format("{\"snippet\": {\"title\": \"%s\",\"description\": \"%d\"}}",
+                .format("{\"snippet\": {\"title\": \"%s\",\"description\": \"%s\"}}",
                         playlist.getTitle(),
                         playlist.getDescription()
                 );
 
-        JsonObject createdList = this.postContent(PLAYLIST_CREATE_ENDPOINT_TEMPLATE, body);
+        JsonObject createdList = this.postContent(
+                PLAYLIST_CREATE_ENDPOINT_TEMPLATE,
+                body,
+                userCredentialsTableEntry.getAccessToken()
+        );
 
         String createdListId = createdList
                 .getAsJsonPrimitive("id")
                 .getAsString();
 
-        this.createPlaylistContent(createdListId, playlist.getTracks());
+        this.createPlaylistContent(createdListId, playlist.getTracks(), userCredentialsTableEntry.getAccessToken());
     }
 
     @Override
-    public String getAuthenticationURL(String userId) throws Exception {
-        return null;
+    public String getAuthenticationURL(String userId) {
+        return String.format(
+                "http://localhost:8080/redirect/%s/%s",
+                this.getServiceDomain(),
+                userId
+        );
     }
 
     @Override
     public UserCredentialsTableEntry getUserCredentials(String userId, String code) throws Exception {
-        return null;
+        String urlRequest = "https://oauth2.googleapis.com/token";
+
+        String url = "code=%s&" +
+                "client_id=837535089878-9d7cck9l04pu9r23r0r06tt9lh2hsquv.apps.googleusercontent.com&" +
+                "client_secret=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&" +
+                "redirect_uri=http%3A//localhost/redirect/youtube&" +
+                "grant_type=authorization_code";
+
+        String body = String.format(url, code);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(urlRequest))
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .POST(HttpRequest.BodyPublishers.ofString(body))
+                .build();
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        JsonObject object = JsonParser
+                .parseString(response.body())
+                .getAsJsonObject();
+
+        String accessToken = object
+                .getAsJsonPrimitive("access_token")
+                .getAsString();
+
+        String refreshToken = object
+                .getAsJsonPrimitive("refresh_token")
+                .getAsString();
+
+        return new UserCredentialsTableEntry(userId, accessToken, refreshToken);
     }
 
     @Override
-    public void refreshCredentials() throws Exception {
+    public void refreshCredentials() {
     }
 
     @Override
@@ -170,11 +216,14 @@ public class YTAPIFacade extends StreamingServiceFacade<YTSong>{
     }
 
 
-    private JsonObject postContent(String endpoint, String body) throws java.io.IOException, InterruptedException {
+    private JsonObject postContent(String endpoint, String body, String token) throws java.io.IOException, InterruptedException {
         String urlRequest = API_BASE_URL + endpoint + API_KEY;
+
+        String bearerToken = String.format("Bearer %s", token);
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(urlRequest))
+                .header("Authorization", bearerToken)
                 .header("Accept", "application/json")
                 .header("Content-Type", "application/json")
                 .POST(HttpRequest.BodyPublishers.ofString(body))
@@ -213,7 +262,7 @@ public class YTAPIFacade extends StreamingServiceFacade<YTSong>{
         return playlistSongs;
     }
 
-    private void createPlaylistContent(String listId, List<Song> songs) throws Exception {
+    private void createPlaylistContent(String listId, List<Song> songs, String token) throws Exception {
         for (Song song: songs) {
             String body = String.format(
                     "{\"snippet\": {\"playlistId\": \"%s\",\"resourceId\": {\"kind\": \"youtube#video\",\"videoId\": \"%s\"}}}",
@@ -221,7 +270,7 @@ public class YTAPIFacade extends StreamingServiceFacade<YTSong>{
                     this.getAssetIdFromURL(this.getSongFromSongObject(song).getUrl())
             );
 
-            this.postContent(PLAYLIST_CREATE_CONTENT_ENDPOINT_TEMPLATE, body);
+            this.postContent(PLAYLIST_CREATE_CONTENT_ENDPOINT_TEMPLATE, body, token);
         }
     }
 }
